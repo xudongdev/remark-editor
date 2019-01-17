9c335bff5deec267319625ca9ffecd42f2b9dbcf
xudong <i@xudong.cc>
2019-01-18 02:06:00 +0800

    Updates

---
+++ precache-manifest.9316202b070004b6523382ad426e4e2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afaffe636dbc8bacf45a",
+    "url": "/static/css/main.59306db4.chunk.css"
+  },
+  {
+    "revision": "afaffe636dbc8bacf45a",
+    "url": "/static/js/main.afaffe63.chunk.js"
+  },
+  {
+    "revision": "a1a9824d009a99a09624",
+    "url": "/static/css/1.4008a797.chunk.css"
+  },
+  {
+    "revision": "a1a9824d009a99a09624",
+    "url": "/static/js/1.a1a9824d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1451a228f5b8f911fbd16a1f3bf7567a",
+    "url": "/index.html"
+  }
+];
