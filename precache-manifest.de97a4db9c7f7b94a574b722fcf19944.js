1d98ef925830e996d47fdd8b95da4a468073642e
xudong <me@xudong.cc>
2019-07-11 11:27:17 +0800

    Updates

---
+++ precache-manifest.de97a4db9c7f7b94a574b722fcf19944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beefc1a3c02858844f76c16309b5d2fc",
+    "url": "/remark-editor/index.html"
+  },
+  {
+    "revision": "a3495e2167155ea08b9f",
+    "url": "/remark-editor/static/css/2.2fff2021.chunk.css"
+  },
+  {
+    "revision": "f33ba8edbaaefbf51e11",
+    "url": "/remark-editor/static/css/main.67ee5ad8.chunk.css"
+  },
+  {
+    "revision": "a3495e2167155ea08b9f",
+    "url": "/remark-editor/static/js/2.7305317c.chunk.js"
+  },
+  {
+    "revision": "f33ba8edbaaefbf51e11",
+    "url": "/remark-editor/static/js/main.1937cf79.chunk.js"
+  },
+  {
+    "revision": "014ce1a36f5e490787f7",
+    "url": "/remark-editor/static/js/runtime~main.af2b1ba6.js"
+  }
+]);
